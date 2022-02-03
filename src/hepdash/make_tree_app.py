27b06a0799fef10c89912f640bbaf0a6b9e81d00
Ethan Lewis Simpson <ethan.simpson@cern.ch>
2022-02-03 12:59:11 +0100

    remvoe uncessary

--- src/hepdash/make_tree_app.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import dataclass
-import itertools
-import yaml
-import os
-import matplotlib.pyplot as plt
-import streamlit as st
-
-from hepdash.apps.Tree_Apps import General,Specific,Preset
-
